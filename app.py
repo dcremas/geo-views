e8ea103b6a34ed9bc8295ed7aebea00eb41c1651
Dustin Cremascoli <dustincremascoli@aol.com>
2023-11-15 13:23:44 -0600

    Some fine tuning.

--- app.py
+++ app.py
@@ -3,8 +3,9 @@ from pathlib import Path
 import pandas as pd
 import geoviews as gv
 
+from bokeh.io import show
 from bokeh.layouts import column
-from bokeh.models import ColumnDataSource, Div, Select, RangeTool, HoverTool
+from bokeh.models import ColumnDataSource, DataTable, DateFormatter, TableColumn, Div
 from bokeh.plotting import figure, curdoc
 
 from geoviews import dim
@@ -13,20 +14,40 @@ gv.extension('bokeh')
 renderer = gv.renderer('bokeh')
 renderer = renderer.instance(mode='server')
 
+desc = Div(text=(Path(__file__).parent / "description.html").read_text("utf8"), sizing_mode="stretch_width",
+           margin=(5, 25, 25, 25))
+
 cities = pd.read_csv('data/cities.csv', encoding="ISO-8859-1")
 population = gv.Dataset(cities, kdims=['Station Name', 'State', 'Year'])
 points = population.to(gv.Points, ['Longitude', 'Latitude'], ['Total Days', 'Station Name', 'State'])
 
 tiles = gv.tile_sources.OSM
 
-layout = tiles * points.opts(
-    color='Total Days', cmap='viridis', size=dim('Total Days')*0.5,
-    tools=['hover'], global_extent=False, width=800, height=600)
+title = "US Cities: Significant Decreases In Hourly Barometric Pressure Bubble Map"
 
-desc = Div(text=(Path(__file__).parent / "description.html").read_text("utf8"), sizing_mode="stretch_width",
-           margin=(2, 2, 2, 15))
+layout = tiles * points.opts(title=title, color='Total Days',
+                             cmap='viridis', size=dim('Total Days')*0.5,
+                             tools=['hover'], global_extent=False,
+                             width=800, height=600)
+
+source = ColumnDataSource(cities)
+
+columns = [
+        TableColumn(field="Station Name", title="Station Name"),
+        TableColumn(field="State", title="State"),
+        TableColumn(field="Latitude", title="Latitude"),
+        TableColumn(field="Longitude", title="Longitude"),
+        TableColumn(field="Year", title="Year"),
+        TableColumn(field="Total Days", title="Total Days"),
+    ]
+
+data_table = DataTable(source=source, columns=columns, width=800, height=600,
+                       margin=(25, 25, 25, 25))
+
 
 curdoc().add_root(column(desc))
 
 doc = renderer.server_doc(layout)
 doc.title = 'GeoViews App'
+
+curdoc().add_root(column(data_table))
